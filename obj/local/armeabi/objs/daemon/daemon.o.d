7e665099f297899e0cf405859f5b77faf146a0cb
carmenloklok <carmenloklok@192.168.0.3>
2012-03-19 19:43:44 +0800

    just a try

---
+++ obj/local/armeabi/objs/daemon/daemon.o.d
@@ -0,0 +1,118 @@
+obj/local/armeabi/objs/daemon/daemon.o: jni/daemon.c \
+ /Users/carmenloklok/work/android-ndk-r7b/platforms/android-14/arch-arm/usr/include/unistd.h \
+ /Users/carmenloklok/work/android-ndk-r7b/platforms/android-14/arch-arm/usr/include/sys/cdefs.h \
+ /Users/carmenloklok/work/android-ndk-r7b/platforms/android-14/arch-arm/usr/include/sys/cdefs_elf.h \
+ /Users/carmenloklok/work/android-ndk-r7b/platforms/android-14/arch-arm/usr/include/sys/types.h \
+ /Users/carmenloklok/work/android-ndk-r7b/platforms/android-14/arch-arm/usr/include/stdint.h \
+ /Users/carmenloklok/work/android-ndk-r7b/platforms/android-14/arch-arm/usr/include/sys/_types.h \
+ /Users/carmenloklok/work/android-ndk-r7b/platforms/android-14/arch-arm/usr/include/machine/_types.h \
+ /Users/carmenloklok/work/android-ndk-r7b/platforms/android-14/arch-arm/usr/include/linux/posix_types.h \
+ /Users/carmenloklok/work/android-ndk-r7b/platforms/android-14/arch-arm/usr/include/linux/stddef.h \
+ /Users/carmenloklok/work/android-ndk-r7b/platforms/android-14/arch-arm/usr/include/linux/compiler.h \
+ /Users/carmenloklok/work/android-ndk-r7b/platforms/android-14/arch-arm/usr/include/asm/posix_types.h \
+ /Users/carmenloklok/work/android-ndk-r7b/platforms/android-14/arch-arm/usr/include/asm/types.h \
+ /Users/carmenloklok/work/android-ndk-r7b/platforms/android-14/arch-arm/usr/include/linux/types.h \
+ /Users/carmenloklok/work/android-ndk-r7b/platforms/android-14/arch-arm/usr/include/machine/kernel.h \
+ /Users/carmenloklok/work/android-ndk-r7b/platforms/android-14/arch-arm/usr/include/sys/sysmacros.h \
+ /Users/carmenloklok/work/android-ndk-r7b/platforms/android-14/arch-arm/usr/include/sys/select.h \
+ /Users/carmenloklok/work/android-ndk-r7b/platforms/android-14/arch-arm/usr/include/sys/time.h \
+ /Users/carmenloklok/work/android-ndk-r7b/platforms/android-14/arch-arm/usr/include/linux/time.h \
+ /Users/carmenloklok/work/android-ndk-r7b/platforms/android-14/arch-arm/usr/include/signal.h \
+ /Users/carmenloklok/work/android-ndk-r7b/platforms/android-14/arch-arm/usr/include/limits.h \
+ /Users/carmenloklok/work/android-ndk-r7b/platforms/android-14/arch-arm/usr/include/sys/limits.h \
+ /Users/carmenloklok/work/android-ndk-r7b/platforms/android-14/arch-arm/usr/include/linux/limits.h \
+ /Users/carmenloklok/work/android-ndk-r7b/platforms/android-14/arch-arm/usr/include/machine/internal_types.h \
+ /Users/carmenloklok/work/android-ndk-r7b/platforms/android-14/arch-arm/usr/include/machine/limits.h \
+ /Users/carmenloklok/work/android-ndk-r7b/platforms/android-14/arch-arm/usr/include/sys/syslimits.h \
+ /Users/carmenloklok/work/android-ndk-r7b/platforms/android-14/arch-arm/usr/include/string.h \
+ /Users/carmenloklok/work/android-ndk-r7b/platforms/android-14/arch-arm/usr/include/malloc.h \
+ /Users/carmenloklok/work/android-ndk-r7b/platforms/android-14/arch-arm/usr/include/asm/signal.h \
+ /Users/carmenloklok/work/android-ndk-r7b/platforms/android-14/arch-arm/usr/include/asm-generic/signal.h \
+ /Users/carmenloklok/work/android-ndk-r7b/platforms/android-14/arch-arm/usr/include/asm/siginfo.h \
+ /Users/carmenloklok/work/android-ndk-r7b/platforms/android-14/arch-arm/usr/include/asm-generic/siginfo.h \
+ /Users/carmenloklok/work/android-ndk-r7b/platforms/android-14/arch-arm/usr/include/sys/sysconf.h \
+ /Users/carmenloklok/work/android-ndk-r7b/platforms/android-14/arch-arm/usr/include/linux/capability.h \
+ /Users/carmenloklok/work/android-ndk-r7b/platforms/android-14/arch-arm/usr/include/pathconf.h \
+ /Users/carmenloklok/work/android-ndk-r7b/platforms/android-14/arch-arm/usr/include/android/log.h \
+ /Users/carmenloklok/work/android-ndk-r7b/platforms/android-14/arch-arm/usr/include/pthread.h \
+ /Users/carmenloklok/work/android-ndk-r7b/platforms/android-14/arch-arm/usr/include/time.h \
+ /Users/carmenloklok/work/android-ndk-r7b/platforms/android-14/arch-arm/usr/include/sched.h \
+ /Users/carmenloklok/work/android-ndk-r7b/platforms/android-14/arch-arm/usr/include/semaphore.h
+
+/Users/carmenloklok/work/android-ndk-r7b/platforms/android-14/arch-arm/usr/include/unistd.h:
+
+/Users/carmenloklok/work/android-ndk-r7b/platforms/android-14/arch-arm/usr/include/sys/cdefs.h:
+
+/Users/carmenloklok/work/android-ndk-r7b/platforms/android-14/arch-arm/usr/include/sys/cdefs_elf.h:
+
+/Users/carmenloklok/work/android-ndk-r7b/platforms/android-14/arch-arm/usr/include/sys/types.h:
+
+/Users/carmenloklok/work/android-ndk-r7b/platforms/android-14/arch-arm/usr/include/stdint.h:
+
+/Users/carmenloklok/work/android-ndk-r7b/platforms/android-14/arch-arm/usr/include/sys/_types.h:
+
+/Users/carmenloklok/work/android-ndk-r7b/platforms/android-14/arch-arm/usr/include/machine/_types.h:
+
+/Users/carmenloklok/work/android-ndk-r7b/platforms/android-14/arch-arm/usr/include/linux/posix_types.h:
+
+/Users/carmenloklok/work/android-ndk-r7b/platforms/android-14/arch-arm/usr/include/linux/stddef.h:
+
+/Users/carmenloklok/work/android-ndk-r7b/platforms/android-14/arch-arm/usr/include/linux/compiler.h:
+
+/Users/carmenloklok/work/android-ndk-r7b/platforms/android-14/arch-arm/usr/include/asm/posix_types.h:
+
+/Users/carmenloklok/work/android-ndk-r7b/platforms/android-14/arch-arm/usr/include/asm/types.h:
+
+/Users/carmenloklok/work/android-ndk-r7b/platforms/android-14/arch-arm/usr/include/linux/types.h:
+
+/Users/carmenloklok/work/android-ndk-r7b/platforms/android-14/arch-arm/usr/include/machine/kernel.h:
+
+/Users/carmenloklok/work/android-ndk-r7b/platforms/android-14/arch-arm/usr/include/sys/sysmacros.h:
+
+/Users/carmenloklok/work/android-ndk-r7b/platforms/android-14/arch-arm/usr/include/sys/select.h:
+
+/Users/carmenloklok/work/android-ndk-r7b/platforms/android-14/arch-arm/usr/include/sys/time.h:
+
+/Users/carmenloklok/work/android-ndk-r7b/platforms/android-14/arch-arm/usr/include/linux/time.h:
+
+/Users/carmenloklok/work/android-ndk-r7b/platforms/android-14/arch-arm/usr/include/signal.h:
+
+/Users/carmenloklok/work/android-ndk-r7b/platforms/android-14/arch-arm/usr/include/limits.h:
+
+/Users/carmenloklok/work/android-ndk-r7b/platforms/android-14/arch-arm/usr/include/sys/limits.h:
+
+/Users/carmenloklok/work/android-ndk-r7b/platforms/android-14/arch-arm/usr/include/linux/limits.h:
+
+/Users/carmenloklok/work/android-ndk-r7b/platforms/android-14/arch-arm/usr/include/machine/internal_types.h:
+
+/Users/carmenloklok/work/android-ndk-r7b/platforms/android-14/arch-arm/usr/include/machine/limits.h:
+
+/Users/carmenloklok/work/android-ndk-r7b/platforms/android-14/arch-arm/usr/include/sys/syslimits.h:
+
+/Users/carmenloklok/work/android-ndk-r7b/platforms/android-14/arch-arm/usr/include/string.h:
+
+/Users/carmenloklok/work/android-ndk-r7b/platforms/android-14/arch-arm/usr/include/malloc.h:
+
+/Users/carmenloklok/work/android-ndk-r7b/platforms/android-14/arch-arm/usr/include/asm/signal.h:
+
+/Users/carmenloklok/work/android-ndk-r7b/platforms/android-14/arch-arm/usr/include/asm-generic/signal.h:
+
+/Users/carmenloklok/work/android-ndk-r7b/platforms/android-14/arch-arm/usr/include/asm/siginfo.h:
+
+/Users/carmenloklok/work/android-ndk-r7b/platforms/android-14/arch-arm/usr/include/asm-generic/siginfo.h:
+
+/Users/carmenloklok/work/android-ndk-r7b/platforms/android-14/arch-arm/usr/include/sys/sysconf.h:
+
+/Users/carmenloklok/work/android-ndk-r7b/platforms/android-14/arch-arm/usr/include/linux/capability.h:
+
+/Users/carmenloklok/work/android-ndk-r7b/platforms/android-14/arch-arm/usr/include/pathconf.h:
+
+/Users/carmenloklok/work/android-ndk-r7b/platforms/android-14/arch-arm/usr/include/android/log.h:
+
+/Users/carmenloklok/work/android-ndk-r7b/platforms/android-14/arch-arm/usr/include/pthread.h:
+
+/Users/carmenloklok/work/android-ndk-r7b/platforms/android-14/arch-arm/usr/include/time.h:
+
+/Users/carmenloklok/work/android-ndk-r7b/platforms/android-14/arch-arm/usr/include/sched.h:
+
+/Users/carmenloklok/work/android-ndk-r7b/platforms/android-14/arch-arm/usr/include/semaphore.h:
